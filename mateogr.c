ceac3f2ce6bddd774b945fa07d889226e6f6a9e0
Grgamrgaa <95843190+Grgamrgaa@users.noreply.github.com>
2021-12-09 08:37:58 +0100

    Add files via upload

---
+++ mateogr.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+int stranicaA=12;
+int stranicaB=8;
+int stranicaC=6;
+printf("Unesi tri stranice\n");
+scanf("%d %d %d"),&stranicaA,&stranicaB,&stranicaC;
+int volumen=stranicaA*stranicaB*stranicaC;
+printf("%d",volumen);
+}
