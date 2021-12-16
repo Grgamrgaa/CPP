67001d2157550993a14bcca104a429b460b1e5e0
Grgamrgaa <95843190+Grgamrgaa@users.noreply.github.com>
2021-12-16 09:34:00 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("površina trokuta je = %d\n",((a*b)/2));
+    return 0;
+}
+
+
