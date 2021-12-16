fac934a458d230d53cdbc413d7db8d1dec6c7056
Grgamrgaa <95843190+Grgamrgaa@users.noreply.github.com>
2021-12-16 10:24:04 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -3,14 +3,10 @@
 
 int main()
 {
-printf("0 && 0 = %d\n",0&&0 );
-printf("0 && 1 = %d\n",0&&1 );
-printf("1 && 0 = %d\n",1&&0 );
-printf("1 && 1 = %d\n",1&&1 );
-printf("0 && 0 = %d\n",0||0 );
-printf("0 && 1 = %d\n",0||1 );
-printf("1 && 0 = %d\n",1||0 );
-printf("1 && 1 = %d\n",1||1 );
+int a=4, b=3;
+ a=b++;
+ b=--a;
+printf("%d %d",a,b);
  return 0;
 
 }
