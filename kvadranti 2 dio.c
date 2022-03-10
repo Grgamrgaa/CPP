51e96900408286656bf3e45e9d62ef647518b988
Grgamrgaa <95843190+Grgamrgaa@users.noreply.github.com>
2022-03-10 08:40:33 +0100

    Add files via upload

---
+++ kvadranti 2 dio.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+ int a;
+
+ printf("Unesi broj kvadranta:");
+ scanf("%d",&a);
+
+ switch(a){
+     case 1:
+     printf("x:+,y:+");
+     break;
+     case 2:
+     printf("x:-,y:+");
+     break;
+     case 3:
+     printf("x:-,y:-");
+     break;
+     case 4:
+     printf("x:+,y:-");
+     break;
+     default:
+        printf("Nepostojeci kvadrant!!!");
+ }
+ return 0;
+}
+
+
+
+
+
+
