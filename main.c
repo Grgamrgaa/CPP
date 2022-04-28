32dc8f96fd8974d63bd8d2c1eb37c537cd4d34e8
Grgamrgaa <95843190+Grgamrgaa@users.noreply.github.com>
2022-04-28 07:42:39 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,37 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main()
-{
+int main(){
 
-    int n, i, fac=1, br=0;
-
-    do{
-    printf("Unesi broj\n");
-    scanf("%d", &n);
-    if(n<0){printf("Error\n");
-    if(br>5) printf("Unesi pozitivan %d\n",br);
-    }
-    br++;
-
-    }while (n<0);
-    i=n;
-    while(i)
-        fac*=i--;
-    printf("faktoriel broja %d je %d",n, fac);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return 0;
+int n,i=0,x=2,y=1,sum=0;
+printf("Unesi koliko ima gradova\n");
+scanf("%d",&n);
+n=n-1;
+while(n>0){
+	n--;
+	printf("Unesi razmak izmedu %d i %d grada \n",y++,x++);
+	scanf("%d",&i);
+	sum+=i;
 }
+printf("%d",sum);
+return 0;
+}
+
+
+
+
+
+
+
+
