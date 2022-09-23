3b54b7890cfb44542746b5a1fb7afd4e403dcc7d
Grgamrgaa <95843190+Grgamrgaa@users.noreply.github.com>
2022-09-23 11:21:58 +0200

    Add files via upload

---
+++ bilježnice i pulpa.c
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int biljeznice(float  n){
+
+return (n*1000)/100;
+}
+
+int main()
+{
+    float kgpulpe;
+    int n;
+    printf ("unesi broj kilogram pulpe ");
+    scanf("%f",&kgpulpe);
+    n=biljeznice(kgpulpe);
+    printf("moze proizvesti %d biljeznica",n);
+
+
+}
+
+
+
+
+
+
