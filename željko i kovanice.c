0ee6e1975570aba1786bdb6d803d2e6a1fe22056
Grgamrgaa <95843190+Grgamrgaa@users.noreply.github.com>
2022-09-23 11:30:37 +0200

    Add files via upload

---
+++ željko i kovanice.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+float novci(int x, float y){
+	return x+0.5*y;
+}
+int main(){
+
+    int a;
+    float b;
+    float c;
+
+    printf("Koliko ima kovanica od 1kn?");
+    scanf("%d",&a);
+    printf("\nkoliko ima kovanica od 0.5kn?"),
+	scanf("%.2f", &b);
+
+	c=novci(a,b);
+
+	printf("\n%f",c);
+
+	return 0;
+}
+
+
+
+
