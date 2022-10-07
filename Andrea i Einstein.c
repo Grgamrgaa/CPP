079ffd20470acfe6465590029c8e1a394a11517c
Grgamrgaa <95843190+Grgamrgaa@users.noreply.github.com>
2022-10-07 10:16:22 +0200

    Add files via upload

---
+++ Andrea i Einstein.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int Mozel(int x){
+return (x+7)>170;}
+
+int main()
+{
+  int x;
+  scanf("%d",&x);
+  printf(Mozel(x)?"moze":"ne moze");
+
+
+
+
+
+
+
+
+
+    return 0;
+}
